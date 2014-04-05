5982c87a185f9e5e851fbc65da2a629434cf5b6f
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2014-04-05 15:43:50 -0400

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VoteATron::Application.config.secret_key_base = '0775f6c994963de3e83d53bba38303d9ce3968fa4ea3dbe0e9d13b8dfd75acd89e5f42f8f72ce671619d2012b917575a759ff341eb508c85cca61ab396155f5a'
